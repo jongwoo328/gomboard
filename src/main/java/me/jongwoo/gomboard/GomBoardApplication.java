3edddc6af57ae0a2ceccf392d3460b1f2b051445
JongwooLee <jongwoo328@gmail.com>
2024-05-11 17:01:32 +0900

    init

---
+++ src/main/java/me/jongwoo/gomboard/GomBoardApplication.java
@@ -0,0 +1,13 @@
+package me.jongwoo.gomboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GomBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jongwoo/gomboard/GomBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jongwoo.gomboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
